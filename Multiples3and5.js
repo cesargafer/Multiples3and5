014b408c41f86e687769cdae0b8c7f22f46f6e06
cesargafer <cesargafer@gmail.com>
2016-09-02 10:23:27 -0500

    functions added

--- Multiples3and5.js
+++ Multiples3and5.js
@@ -2,14 +2,20 @@
 
 // Multiples of 3 and 5
 
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var arr = [];
+function Array(){
+ for(var i = 0; i <= 100; i++){
+  arr.push(i);
+ }
+return arr;
+}
 
+console.log(Array());
 var arrMultiplos = [];
 
 arr.map(function(num){
   if(num % 3 === 0){
    arrMultiplos.push(num);
-  // return console.log(arrMultiplos);
   } else if (num % 5 === 0){
    arrMultiplos.push(num);
   }
@@ -18,7 +24,7 @@ arr.map(function(num){
 );
 
 
-console.log('Los multiplos de 3 & 5 en ' + arr + ' son ' + arrMultiplos);
+console.log('Los multiplos de 3 & 5 en un arry de 100 son ' + arrMultiplos);
 
 
 
