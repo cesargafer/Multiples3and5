0bb475800c0a468af5bdc1033e06d55be1bf09ac
WLjJsJa <wljjsja@yahoo.com>
2016-09-05 10:39:22 -0500

    Created new file Multiples3and5.js

---
+++ Multiples3and5.js
@@ -0,0 +1,46 @@
+// Multiples of 3 and 5
+
+var arr = [];
+function Array(){
+ for(var i = 0; i < 1000; i++){
+  arr.push(i);
+ }
+return arr;
+}
+
+Array();
+
+var arrMultiples = [];
+
+arr.map(function(num){
+  if(num % 3 === 0){
+   arrMultiples.push(num);
+  } else if (num % 5 === 0){
+   arrMultiples.push(num);
+  }
+ return arrMultiples; 
+ }
+);
+
+function Counter(){
+	this.sum = 0;
+}
+
+Counter.prototype.add = function(array) {
+	array.forEach(function(item){
+		this.sum += item;
+	}, this);
+};
+
+var sumOfMultiples = new Counter();
+sumOfMultiples.add(arrMultiples);
+
+
+console.log('The sum of multiples is :' + sumOfMultiples.sum);
+
+
+
+
+
+
+
